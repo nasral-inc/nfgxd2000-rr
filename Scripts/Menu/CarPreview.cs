3303a3def9051497bbbbbf240938d78f7fba9358
TolyaGosuslugi <nikita2010belov@gmail.com>
2024-12-07 19:36:42 +0500

    еще мюгюшака с настольками не работет

---
+++ Scripts/Menu/CarPreview.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CarPreview : MonoBehaviour
+{
+    void Start()
+    {
+        // previewInstance.transform.SetParent(transform);
+        // previewInstance.transform.localPosition = Vector3.zero;
+    }
+}
