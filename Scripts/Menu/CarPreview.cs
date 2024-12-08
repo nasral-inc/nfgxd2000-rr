6e037dd63131d249c58947b30c32b91149dba997
TolyaGosuslugi <nikita2010belov@gmail.com>
2024-12-08 22:14:19 +0500

    menu sdelal

--- Scripts/Menu/CarPreview.cs
+++ Scripts/Menu/CarPreview.cs
@@ -7,14 +7,10 @@ public class CarPreview : MonoBehaviour
 {
     public Settings settings;
     private GameObject prefab;
-    //private GameObject previewInstance;
     private GameObject instance;
 
     void Spawn() {
         prefab = settings.MainPrefab;
-        // previewInstance = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
-        // previewInstance.transform.SetParent(transform);
-        // previewInstance.transform.localPosition = Vector3.zero;
         instance = Instantiate(prefab, transform);
         instance.transform.localPosition = Vector3.zero;
         Rigidbody rigidBody = instance.GetComponent<Rigidbody>();
@@ -25,14 +21,6 @@ public class CarPreview : MonoBehaviour
     void Start()
     {
         Spawn();
-        // prefab = settings.MainPrefab;
-        // previewInstance = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
-        // previewInstance.transform.SetParent(transform);
-        // previewInstance.transform.localPosition = Vector3.zero;
-        // Rigidbody rigidBody = previewInstance.GetComponent<Rigidbody>();
-        // BoxCollider boxCollider = previewInstance.GetComponent<BoxCollider>();
-        // Destroy(rigidBody);
-        // Destroy(boxCollider);
     }
     void FixedUpdate() {
         instance.transform.Rotate(transform.rotation.x, transform.rotation.y, transform.rotation.z+1);
