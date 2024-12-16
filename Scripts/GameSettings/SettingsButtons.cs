5fa99e616e5d7e5ba38b29611c9830e08a7dbb64
TolyaGosuslugi <nikita2010belov@gmail.com>
2024-12-16 21:27:44 +0500

    меню доделано, ураура, осталось стивхуйсмобиль и заебок

--- Scripts/GameSettings/SettingsButtons.cs
+++ Scripts/GameSettings/SettingsButtons.cs
@@ -8,10 +8,18 @@ public class SettingsButtons : MonoBehaviour
 
     public GameObject Penis;
     public GameObject Shkoda;
+    public GameObject Arbuz;
+    public GameObject Printer;
     public void PenisMobil() {
         settings.MainPrefab = Penis;
     }
     public void ShkodaMobil() {
         settings.MainPrefab = Shkoda;
+    }
+    public void ArbuzMobil() {
+        settings.MainPrefab = Arbuz;
+    }
+    public void PrinterMobil() {
+        settings.MainPrefab = Printer;
     }
 }
